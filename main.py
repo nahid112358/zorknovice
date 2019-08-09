21e8895ca0bfdd897178cef3474b2c350dc96145
nahidul112358 <53710988+nahidul112358@users.noreply.github.com>
2019-08-09 17:48:42 +0600

    Update main.py
    Code scribbling!

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
+from sys  import exit
+
 def lighthouse():
     print("Entry restricted!")
-        
+    exit(0)
+
+def west():
+    while True:
+        print("Where do you want to move from here?")
+        move = input("Enter 'up' or 'down': ")
+        if "up" in move:
+            boom()
+        else:
+            groom()
+def groom():
+    print("Welcome to the underground kingdom!")
+def boom():
+    print("Welcome to the sky, are you ready to fly?")
+west()
