7013ffb2665af68c5ad1d7bfa794e7d6fbcd5c3d
nahidul112358 <53710988+nahidul112358@users.noreply.github.com>
2019-08-09 17:19:20 +0600

    Update main.py
    Code scribbling!

--- main.py
+++ main.py
@@ -1,3 +1,40 @@
+from sys  import exit
+
 def lighthouse():
     print("Entry restricted!")
-        
+    exit(0)
+def north():
+    while True:
+        print("Where do you want to move from here?")
+        move = input(""Enter 'up' or 'down': "")
+        if "up" in move:
+            boom()
+        else:
+            groom()
+def south():
+    while True:
+        print("Where do you want to move from here?")
+        move = input(""Enter 'up' or 'down': "")
+        if "up" in move:
+            boom()
+        else:
+            groom()
+
+def east():
+    while True:
+        print("Where do you want to move from here?")
+        move = input("Enter 'up' or 'down': ")
+        if "up" in move:
+            boom()
+        else:
+            groom()
+def west():
+    while True:
+        print("Where do you want to move from here?")
+        move = input(""Enter 'up' or 'down': "")
+        if "up" in move:
+            boom()
+        else:
+            groom()
+
+            
